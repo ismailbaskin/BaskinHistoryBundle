61d03930a535252d5a7e643f28242fed5ea28117
İsmail BASKIN <ismailbaskin1@gmail.com>
2015-12-16 14:57:15 +0200

    Remove unused use statement fix #3

--- DependencyInjection/BaskinHistoryExtension.php
+++ DependencyInjection/BaskinHistoryExtension.php
@@ -3,7 +3,6 @@
 namespace Baskin\HistoryBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
